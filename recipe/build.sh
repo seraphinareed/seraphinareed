d9c1ae3b146435b8222df29963becfef9f8b7992
seraphinareed <li01535zhang415@163.com>
2024-06-13 16:24:06 +0800

    recipe/build.sh: fix configuration on GCC7 toolchain

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,9 @@ if [[ "$(uname)" = Darwin ]] ; then
     export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
 fi
 
+# Needed for jpeg on Linux/GCC7:
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+
 ./configure \
     --prefix=$PREFIX \
     --libdir=$PREFIX/lib \
