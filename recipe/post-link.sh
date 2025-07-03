7ada781866122832e83a18f3979449273e681dbf
seraphinareed <li01535zhang415@163.com>
2025-07-04 00:30:07 +0800

    Allow post-link.sh to fail

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # Make sure that gdk-pixbuf's loaders.cache is fully up-to-date. We don't want
 # to assume that pkg-config is available, so we hack a bit to avoid running it
@@ -9,13 +9,18 @@
 # available as `$PREFIX/bin/.gdk-pixbuf-post-link.sh`.
 
 set -e
-eval $(grep -v : $PREFIX/lib/pkgconfig/gdk-pixbuf-2.0.pc)
+eval $(grep -v : "$PREFIX/lib/pkgconfig/gdk-pixbuf-2.0.pc")
 if [ -z "$gdk_pixbuf_cache_file" ] ; then
-    bindir="$(echo $PREFIX/lib/gdk-pixbuf-2.0/*)"
-    if [ ! -d $bindir ] ; then
+    bindir="$(echo "${PREFIX}/lib/gdk-pixbuf-2.0/"*)"
+    if [ ! -d "$bindir" ] ; then
        echo >&2 "error: no such gdk-pixbuf binary directory $PREFIX/lib/gdk-pixbuf-2.0/*"
        exit 1
     fi
     gdk_pixbuf_cache_file="$bindir/loaders.cache"
 fi
-"$PREFIX/bin/gdk-pixbuf-query-loaders" >$gdk_pixbuf_cache_file
+"$PREFIX/bin/gdk-pixbuf-query-loaders" >"$gdk_pixbuf_cache_file" 2>>"${PREFIX}/.messages.txt" || \
+(
+    echo "ERROR: Failed to update gdk-pixbuf's cache, some plugins may be be found."
+    echo "To fix this, activate the environment and run:"
+    echo "    gdk-pixbuf-query-loaders >\"$gdk_pixbuf_cache_file\""
+) >> "${PREFIX}/.messages.txt"
